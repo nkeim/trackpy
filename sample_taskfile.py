d924c71eb90ecd694bb6531b9e8e2661ff03584f
Nathan Keim <nkeim@seas.upenn.edu>
2013-08-07 08:50:28 -0400

    Denoted taskfile as a sample.

---
+++ sample_taskfile.py
@@ -0,0 +1,27 @@
+# The package will provide a function to (re)load this file and extract this function.
+from tasker import TaskMan, JSON, Pandas
+import pandas
+
+class MovieTasker(TaskMan):
+    # This could have @cachedprop attributes, basic metadata, etc.
+    pass
+def enter(dirname):
+    task = MovieTasker(dirname)
+    # Config may be set right here, or by MovieTasker.__init__(), or both.
+    task.conf = dict(one='one', two=2.0, name=task.name)
+    # We define the tasks here so that we can use closures for maximum
+    # directory-specific flexibilty
+    def one(ins):
+        """Docstring: One"""
+        task.one_count += 1
+        return task.conf['one'] # Goes to JSON
+    @task(one, [JSON('two.json'), JSON('2b.json')])
+    def two(input):
+        return task.conf['two'], {'twofloat': task.conf['two'], 
+                'onestr': input, 'name': task.conf['name']}
+    @task([one, two], Pandas('three.h5'))
+    def three(ins):
+        twofloat = ins[1][1]['twofloat']
+        return pandas.Series([twofloat,])
+    #... now available as task.one(), task.one.refresh(), etc.
+    return task # Very important!
