e31aba6e7b2f5a4e6218fbb565177adaede92e0b
Nathan Keim <nkeim@seas.upenn.edu>
2013-08-25 17:29:44 -0400

    Tracking unit tests now run again; memory test fails.

--- trackpy/tests/test_more.py
+++ trackpy/tests/test_more.py
@@ -1,5 +1,5 @@
 import numpy as np
-from trackpy.tracking import PointND, link, Hash_table
+from trackpy.tracking import PointND, link
 from copy import deepcopy
 
 # Call lambda function for a fresh copy each time.
@@ -12,19 +12,15 @@ max_disp = np.diff(random_x).max()
 random_walk = lambda: [[PointND(t, (x, 5))] for t, x in enumerate(random_x)]
 
 
-def hash_generator(dims, box_size):
-    return lambda: Hash_table(dims, box_size)
-
-
 def test_search_range():
-    t = link(unit_steps(), 1.1, hash_generator((10, 10), 1))
+    t = link(unit_steps(), 1.1)
     assert len(t) == 1  # One track
-    t_short = link(unit_steps(), 0.9, hash_generator((10, 10), 1))
+    t_short = link(unit_steps(), 0.9)
     assert len(t_short) == len(unit_steps())  # Each step is a separate track.
 
-    t = link(random_walk(), max_disp + 0.1, hash_generator((10, 10), 1))
+    t = link(random_walk(), max_disp + 0.1)
     assert len(t) == 1  # One track
-    t_short = link(random_walk(), max_disp - 0.1, hash_generator((10, 10), 1))
+    t_short = link(random_walk(), max_disp - 0.1)
     assert len(t_short) > 1  # Multiple tracks
 
 
@@ -37,20 +33,8 @@ def test_memory():
     gapped = lambda: deepcopy([[a[0], b[0]], [a[1], b[1]], [a[2]],
                               [a[3], b[3]], [a[4], b[4]]])
     safe_disp = 1 + random_x.max() - random_x.min()  # Definitely large enough
-    t0 = link(gapped(), safe_disp, hash_generator((10, 10), 1), memory=0)
+    t0 = link(gapped(), safe_disp, memory=0)
     assert len(t0) == 3, len(t0)
-    t5 = link(gapped(), safe_disp, hash_generator((10, 10), 1), memory=5)
+    t5 = link(gapped(), safe_disp, memory=5)
     assert len(t5) == 2, len(t5)
 
-
-def test_box_size():
-    """No matter what the box size, there should be one track, and it should
-    contain all the points."""
-    for box_size in [0.1, 1, 10]:
-        t1 = link(unit_steps(), 1.1, hash_generator((10, 10), box_size))
-        t2 = link(random_walk(), max_disp + 1,
-                  hash_generator((10, 10), box_size))
-        assert len(t1) == 1
-        assert len(t2) == 1
-        assert len(t1[0].points) == len(unit_steps())
-        assert len(t2[0].points) == len(random_walk())
