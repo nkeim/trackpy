746ec47808f5c40420c3a1bcbc7a87fef17ab58e
Nathan Keim <nkeim@seas.upenn.edu>
2014-03-16 16:37:21 -0400

    Added setup.py, etc.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='tasker',
+    version='0.1dev',
+    author='Nathan C. Keim',
+    author_email='nkeim@seas.upenn.edu',
+    #url='https://github.com/nkeim/tasker',
+    packages=['tasker'],
+    install_requires=['path.py',],
+    test_suite = 'nose.collector'
+    )
