f683b367626963274c8832f89977e8af028c51a6
Nathan Keim <nkeim@seas.upenn.edu>
2014-04-04 11:27:24 -0400

    API: use() searches upward for taskfile_sub.py

--- tasker/loader.py
+++ tasker/loader.py
@@ -1,14 +1,16 @@
 import imp, hashlib, sys
 from path import path
 
-def use(directory, taskfile='taskfile.py', **kw):
+def use(directory, taskfile='taskfile.py', taskfile_sub='taskfile_sub.py', **kw):
     """Return an object for 'directory' that gives access to its tasks.
 
     This is an instance of Tasker (or a subclass thereof) that is defined and
     set up in the use() function, contained in 'taskfile' (default "taskfile.py").
 
-    Searches upward from 'directory' to find the relevant 'taskfile'. 'taskfile'
-    can be a glob pattern or an absolute path, in which case no search is performed.
+    If 'taskfile' is not in 'directory', searches upward from 'directory' to find
+    a file named 'taskfile_sub' (default "taskfile_sub.py"). 'taskfile' and
+    'taskfile_sub' can be glob patterns. They can also be absolute paths,
+    in which case no search is performed.
     
     The use() that is found is called with 'directory' and any additional keyword 
     arguments.
@@ -20,18 +22,20 @@ def use(directory, taskfile='taskfile.py', **kw):
         sys.stderr.write('Problem calling use() for %s\n' % directory)
         raise
 
-def taskmod(directory, taskfile='taskfile.py'):
+def taskmod(directory, taskfile='taskfile.py', taskfile_sub='taskfile_sub.py'):
     """Import the module that defines tasks (and anything else) for 'directory'.
 
-    Searches upward from 'directory' to find the relevant 'taskfile'. 'taskfile'
-    can be a glob pattern or an absolute path, in which case no search is performed.
+    If 'taskfile' is not in 'directory', searches upward from 'directory' to find
+    a file named 'taskfile_sub' (default "taskfile_sub.py"). 'taskfile' and
+    'taskfile_sub' can be glob patterns. They can also be absolute paths,
+    in which case no search is performed.
     """
-    tf = which(directory, taskfile=taskfile)
+    tf = which(directory, taskfile=taskfile, taskfile_sub=taskfile_sub)
     # Assign a unique module name, to avoid trouble
     tf_hash = hashlib.sha1(str(tf)).hexdigest()[:10]
     return imp.load_source('_taskfile_' + tf_hash, tf)
 
-def which(directory, taskfile='taskfile.py'):
+def which(directory, taskfile='taskfile.py', taskfile_sub='taskfile_sub.py'):
     """Returns path to the Python file defining tasker tasks for 'directory'.
 
     'taskfile' is the glob pattern to match with filenames, or an absolute path.
@@ -39,20 +43,31 @@ def which(directory, taskfile='taskfile.py'):
     if path(taskfile).isabs(): 
         return path(taskfile)
     dirpath = path(directory).abspath()
-    tf = _find_upward(dirpath, taskfile)
-    if tf is None:
-        raise IOError('Task definitions ("%s") not found in %s or any directory above it' \
-                % (taskfile, dirpath))
-    return tf
+    tfs_local = dirpath.glob(taskfile)
+    if len(tfs_local) == 0:
+        tf = _find_upward(dirpath.parent, taskfile_sub)
+        if tf is None:
+            raise IOError('Task definitions not found in %s or any directory above it' \
+                    % (dirpath))
+        else:
+            return tf
+    elif len(tfs_local) == 1:
+        return tfs_local[0]
+    else:
+        raise IOError('Found multiple matches for %s in %s: %r' \
+                      % (taskfile, dirpath, tfs_local))
 
 def _find_upward(dirpath, pattern):
     """Search for glob pattern 'pattern' going up the directory tree.
     
     Returns path of first file found, or None if the top of the tree was reached.
+    If 'pattern' is an absolute pathname, does not perform a search.
     
     Raises exception if multiple matching files are found in a single directory."""
+    if path(pattern).isabs():
+        return path(pattern)
     p = dirpath
-    while p != p.parent: # Stop when we reach the root path
+    while p != p.parent: # Stop when we can't go up any further (at root path)
         tfs = p.glob(pattern)
         if len(tfs) == 0:
             p = p.parent
--- tasker/tests/test_loader.py
+++ tasker/tests/test_loader.py
@@ -11,7 +11,7 @@ class loadertests(unittest.TestCase):
     def setUp(self):
         os.chdir(basedir)
         self.testdir = path(tempfile.mkdtemp())
-        self.taskfile = self.testdir / 'taskfile.py'
+        self.taskfile = self.testdir / 'taskfile_sub.py'
         sample_taskfile.copy(self.taskfile)
         self.temptaskdir = self.testdir / 'top' / 'middle' / 'bottom'
         self.temptaskdir.makedirs_p()
@@ -19,10 +19,18 @@ class loadertests(unittest.TestCase):
         os.chdir(basedir)
         self.testdir.rmtree()
     def test_loader(self):
+        assert loader.which(self.temptaskdir).basename() == 'taskfile_sub.py'
         task = loader.use(self.temptaskdir)
         self.assertEqual(task.two()[0], 2.0)
-    def test_loaderfail(self):
+    def test_loader_fail(self):
         self.taskfile.unlink()
         with self.assertRaises(IOError):
             loader.use(self.temptaskdir)
+    def test_local_taskfile(self):
+        local_taskfile = self.temptaskdir / 'taskfile.py'
+        sample_taskfile.copy(local_taskfile)
+        assert loader.which(self.temptaskdir).basename() == 'taskfile.py'
+        task = loader.use(self.temptaskdir)
+        self.assertEqual(task.two()[0], 2.0)
+        local_taskfile.unlink()
 
