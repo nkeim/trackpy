83b0e5e85b426999160160d5bdf25a179ef8918f
Nathan Keim <nkeim@seas.upenn.edu>
2014-04-27 21:23:58 -0400

    Added Pickle storage class

--- tasker/__init__.py
+++ tasker/__init__.py
@@ -1,4 +1,4 @@
 from .base import isUpToDate, cachedprop
 from .task import Tasker, LockException
-from .storage import Pandas, JSON
+from .storage import Pandas, JSON, Pickle
 from .loader import use, taskmod
